fa053a06f5f909e8f3c70339629824841e30a394
tkhs0604 <43767445+tkhs0604@users.noreply.github.com>
2020-04-19 01:41:45 +0900

    change to returning random number of items

--- app/src/main/java/com/tkhs0604/databindingrecyclerviewsample/main/MainRepository.kt
+++ app/src/main/java/com/tkhs0604/databindingrecyclerviewsample/main/MainRepository.kt
@@ -1,11 +1,19 @@
 package com.tkhs0604.databindingrecyclerviewsample.main
 
 import com.tkhs0604.databindingrecyclerviewsample.main.model.MainCell
+import kotlin.random.Random
 
 class MainRepository {
 
     fun loadItems(): List<MainCell> {
-        return listOf(
+        return items
+    }
+
+    private val items: List<MainCell>
+        get() = samples.take(Random.nextInt(samples.size))
+
+    companion object {
+        private val samples = listOf(
             MainCell.Cell1("Taro"),
             MainCell.Cell2(10000),
             MainCell.Cell1("Jiro"),
